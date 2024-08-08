b5a8339e0d52f43cd51d1afb06756ce3b17fbf1a
Vitoria Luna! <isabeli.vitoria.luna@gmail.com>
2024-08-08 11:46:12 -0300

    Update EXERCICIO1.js
    atualizado

--- EXERCICIO1.js
+++ EXERCICIO1.js
@@ -1,10 +1,6 @@
 let valor1 = 10
 let valor2 = 24
 
-if (valor1 > valor2) {
+if (valor1 < valor2) {
     console.log(`O maior valor é o valor ${valor1}`);
 } 
-
-if (valor2 > valor1) {
-    console.log(`O maior valor é o valor ${valor2}`);
-}
